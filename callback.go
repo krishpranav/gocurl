9037bd5f775a66eb6e7845c7b8eebd204d6a78ac
krishpranav <krisna.pranav@gmail.com>
2021-11-27 12:59:36 +0530

    callback funcs

--- callback.go
+++ callback.go
@@ -1,7 +1,10 @@
 package curl
 
 import "C"
-import "unsafe"
+
+import (
+	"unsafe"
+)
 
 func goCallHeaderFunction(ptr *C.char, size C.size_t, ctx unsafe.Pointer) uintptr {
 	curl := context_map.Get(uintptr(ctx))
@@ -10,4 +13,35 @@ func goCallHeaderFunction(ptr *C.char, size C.size_t, ctx unsafe.Pointer) uintpt
 		return uintptr(size)
 	}
 	return C.CURL_WRITEFUNC_PAUSE
+}
+
+func goCallWriteFunction(ptr *C.char, size C.size_t, ctx unsafe.Pointer) uintptr {
+	curl := context_map.Get(uintptr(ctx))
+	buf := C.GoBytes(unsafe.Pointer(ptr), C.int(size))
+	if (*curl.writeFunction)(buf, curl.writeData) {
+		return uintptr(size)
+	}
+	return C.CURL_WRITEFUNC_PAUSE
+}
+
+func goCallProgressFunction(dltotal, dlnow, ultotal, ulnow C.double, ctx unsafe.Pointer) int {
+	curl := context_map.Get(uintptr(ctx))
+	if (*curl.progressFunction)(float64(dltotal), float64(dlnow),
+		float64(ultotal), float64(ulnow),
+		curl.progressData) {
+		return 0
+	}
+	return 1
+}
+
+func goCallReadFunction(ptr *C.char, size C.size_t, ctx unsafe.Pointer) uintptr {
+	curl := context_map.Get(uintptr(ctx))
+	buf := C.GoBytes(unsafe.Pointer(ptr), C.int(size))
+	ret := (*curl.readFunction)(buf, curl.readData)
+	str := C.CString(string(buf))
+	defer C.free(unsafe.Pointer(str))
+	if C.memcpy(unsafe.Pointer(ptr), unsafe.Pointer(str), C.size_t(ret)) == nil {
+		panic("read_callback memcpy error!")
+	}
+	return uintptr(ret)
 }
