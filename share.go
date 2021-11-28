eac86b4cbd3bdb9ba0bd5411029978e75d796f3a
krishpranav <krisna.pranav@gmail.com>
2021-11-28 12:59:07 +0530

    share

--- share.go
+++ share.go
@@ -1,6 +1,7 @@
 package curl
 
 import "C"
+
 import "unsafe"
 
 type CurlShareError C.CURLMcode
@@ -14,7 +15,6 @@ func newCurlShareError(errno C.CURLSHcode) error {
 	if errno == 0 {
 		return nil
 	}
-
 	return CurlShareError(errno)
 }
 
@@ -25,4 +25,25 @@ type CURLSH struct {
 func ShareInit() *CURLSH {
 	p := C.curl_share_init()
 	return &CURLSH{p}
+}
+
+func (shcurl *CURLSH) Cleanup() error {
+	p := shcurl.handle
+	return newCurlShareError(C.curl_share_cleanup(p))
+}
+
+func (shcurl *CURLSH) Setopt(opt int, param interface{}) error {
+	p := shcurl.handle
+	if param == nil {
+		return newCurlShareError(C.curl_share_setopt_pointer(p, C.CURLSHoption(opt), nil))
+	}
+	switch opt {
+
+	case SHOPT_SHARE, SHOPT_UNSHARE:
+		if val, ok := param.(int); ok {
+			return newCurlShareError(C.curl_share_setopt_long(p, C.CURLSHoption(opt), C.long(val)))
+		}
+	}
+	panic("not supported CURLSH.Setopt opt or param")
+	return nil
 }
